d2d1a8ea6ea528741dee2ec470dd509bf824c56b
antoineabourjeily <antoineabourjeily@gmail.com>
2021-12-09 11:47:35 +0100

    from gender code

--- TD-Solutions/efrei/src/efrei/td5/Gender.java
+++ TD-Solutions/efrei/src/efrei/td5/Gender.java
@@ -1,13 +1,22 @@
 package efrei.td5;
 
 public enum Gender {
-	FEMALE((byte)2),
-	MALE((byte)1),
-	NON_BINARY((byte)3),
-	OTHER((byte)4);
-	
+	FEMALE((byte) 2), 
+	MALE((byte) 1), 
+	NON_BINARY((byte) 3), 
+	OTHER((byte) 4);
+
 	public final byte genderCode;
+
 	private Gender(byte genderCode) {
 		this.genderCode = genderCode;
+	}
+
+	public static Gender fromGenderCode(byte genderCode) {
+		for (Gender g : Gender.values()) {
+			if (g.genderCode == genderCode)
+				return g;
+		}
+		return OTHER;
 	}
 }
